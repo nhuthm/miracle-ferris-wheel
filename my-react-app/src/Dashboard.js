1cb6721906a9de5051f1ed7522f1fb61675f9694
Nhut Huynh <nhuthuynh.steve@setel.com>
2023-11-08 21:44:17 +0700

    chore: finish update dashboard page UI

--- my-react-app/src/Dashboard.js
+++ my-react-app/src/Dashboard.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import './Dashboard.css';
+
+import TabGallery from './components/tab-gallery/tab-gallery';
+import hadilao from './images/hadilao.png';
+import manwah from './images/manwah.jpeg';
+import unatoto from './images/unatoto.jpeg';
+import unatoto1 from './images/unatoto.jpeg';
+
+const collection = [
+  { src: hadilao, caption: "Caption eleven" },
+  { src: manwah, caption: "Caption twelve" },
+  { src: unatoto, caption: "Caption thirteen" },
+  { src: unatoto1, caption: "Caption thirteen" },
+  { src: hadilao, caption: "Caption eleven" },
+  { src: manwah, caption: "Caption twelve" },
+  { src: unatoto, caption: "Caption thirteen" },
+  { src: unatoto1, caption: "Caption thirteen" }
+];
+
 
 function Dashboard() {
   return (
-    <div>
-      <h2>Welcome to the Dashboard!</h2>
+    <div className="Dashboard">
+      <h2>Dinner Location Collection</h2>
+      <p>Click on each image below to show the corresponding image.</p>
+
+      <TabGallery input={collection} ratio={`10:10`} />
     </div>
   );
 }
